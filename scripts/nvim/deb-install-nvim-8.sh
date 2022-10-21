bc5c2aa20161dc8ee2b6d2c3e3ed67441edbf059
Matt <mucha.mateusz@hotmail.com>
2022-10-21 12:43:24 +0200

    Update deb-install-nvim-8.sh

--- scripts/nvim/deb-install-nvim-8.sh
+++ scripts/nvim/deb-install-nvim-8.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # install nvim 8 in debian
-wget https://github.com/Zaruun/my-config/blob/main/scripts/nvim/nvim-linux64.deb
+wget https://github.com/neovim/neovim/releases/download/v0.8.0/nvim-linux64.deb
 sudo apt install ./nvim-linux64.deb
 # rm nvim-linux64.deb
 
