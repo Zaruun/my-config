3553309c2f9ea1fad8f8ebed03301f47469ec637
Matt <mucha.mateusz@hotmail.com>
2022-10-22 14:17:34 +0200

    debian.sh

---
+++ scripts/Readme.md
@@ -0,0 +1,6 @@
+# Quick links
+---
+### One link config - Debian/Ubuntu
+```
+sh -c "$(wget https://raw.githubusercontent.com/Zaruun/my-config/main/scripts/debian.sh -O -)"
+```
---
+++ scripts/debian.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+# Update and Upgrade
+sudo apt update -y
+sudo apt upgrade -y
+
+# Install core-tools
+sh -c "$(wget https://raw.githubusercontent.com/Zaruun/my-config/main/scripts/core-tools/core-tools.sh -O -)"
+
+# Install and config zsh
+sh -c "$(wget https://raw.githubusercontent.com/Zaruun/my-config/main/scripts/zsh/zsh-install-deb.sh -O -)"
+
+# Install NeoVim 0.8.0
+sh -c "$(wget https://raw.githubusercontent.com/Zaruun/my-config/main/scripts/nvim/deb-install-nvim-8.sh -O -)"
+
+# Configure nvim
+sh -c "$(wget https://raw.githubusercontent.com/Zaruun/my-config/main/scripts/nvim/nvim-install-config.sh -O -)"
