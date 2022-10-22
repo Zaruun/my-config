a8885eb047220739f68837ee0eedf2441efb8d85
Matt <mucha.mateusz@hotmail.com>
2022-10-22 14:40:38 +0200

    ok

--- scripts/nvim/nvim-install-config.sh
+++ scripts/nvim/nvim-install-config.sh
@@ -2,6 +2,11 @@
 
 git clone https://github.com/zaruun/my-config
 
+if [ ! -d ~/.config/nvim ] then
+    mkdir ~/.config
+    mkdir ~/.config/nvim
+fi
+
 rm -rf ~/.config/nvim
 
 cd ~/my-config/dotfiles
