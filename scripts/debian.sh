148daff0387e58cea6c4f014ff3f321149b2e09d
Matt <mucha.mateusz@hotmail.com>
2022-10-22 14:25:32 +0200

    Update debian.sh

--- scripts/debian.sh
+++ scripts/debian.sh
@@ -4,14 +4,44 @@
 sudo apt update -y
 sudo apt upgrade -y
 
-# Install core-tools
-sh -c "$(wget https://raw.githubusercontent.com/Zaruun/my-config/main/scripts/core-tools/core-tools.sh -O -)"
+sudo apt install -y wget
 
-# Install NeoVim 0.8.0
-sh -c "$(wget https://raw.githubusercontent.com/Zaruun/my-config/main/scripts/nvim/deb-install-nvim-8.sh -O -)"
+sudo apt install -y curl
 
-# Configure nvim
-sh -c "$(wget https://raw.githubusercontent.com/Zaruun/my-config/main/scripts/nvim/nvim-install-config.sh -O -)"
+sudo apt install -y git
 
-# Install and config zsh
-sh -c "$(wget https://raw.githubusercontent.com/Zaruun/my-config/main/scripts/zsh/zsh-install-deb.sh -O -)"
+sudo apt install -y htop
+
+sudo apt install -y tmux
+
+sudo apt install -y neofetch
+
+sudo apt install -y mc
+
+wget https://github.com/neovim/neovim/releases/download/v0.8.0/nvim-linux64.deb
+sudo apt install ./nvim-linux64.deb
+sudo rm -f ./nvim-linux64.deb
+
+echo 'NeoVim 0.8.0 installed successfully!'
+
+
+git clone https://github.com/zaruun/my-config
+
+rm -rf ~/.config/nvim
+
+cd ~/my-config/dotfiles
+
+mv nvim ~/.config/nvim
+cd ~
+rm -rf my-config
+
+# install zsh
+sudo apt install zsh -y
+
+# install oh-my-zsh
+sh -c "$(wget https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh -O -)"
+
+# config .zshrc
+cd ~
+sed -i 's/robbyrussell/bureau/' ~/.zshrc
+source ~/.zshrc
