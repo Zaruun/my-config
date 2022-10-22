e179ab7584025071c3733e8f9c7dd3bd4db4c9bc
Matt <mucha.mateusz@hotmail.com>
2022-10-22 14:20:14 +0200

    Update debian.sh

--- scripts/debian.sh
+++ scripts/debian.sh
@@ -7,11 +7,11 @@ sudo apt upgrade -y
 # Install core-tools
 sh -c "$(wget https://raw.githubusercontent.com/Zaruun/my-config/main/scripts/core-tools/core-tools.sh -O -)"
 
-# Install and config zsh
-sh -c "$(wget https://raw.githubusercontent.com/Zaruun/my-config/main/scripts/zsh/zsh-install-deb.sh -O -)"
-
 # Install NeoVim 0.8.0
 sh -c "$(wget https://raw.githubusercontent.com/Zaruun/my-config/main/scripts/nvim/deb-install-nvim-8.sh -O -)"
 
 # Configure nvim
 sh -c "$(wget https://raw.githubusercontent.com/Zaruun/my-config/main/scripts/nvim/nvim-install-config.sh -O -)"
+
+# Install and config zsh
+sh -c "$(wget https://raw.githubusercontent.com/Zaruun/my-config/main/scripts/zsh/zsh-install-deb.sh -O -)"
