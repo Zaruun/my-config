d6d06164cb249dec29978662cde70132f76e4d7b
Matt <mucha.mateusz@hotmail.com>
2022-10-22 14:07:56 +0200

    Update core-tools.sh

--- scripts/core-tools/core-tools.sh
+++ scripts/core-tools/core-tools.sh
@@ -11,8 +11,14 @@ sudo apt install -y htop
 sudo apt install -y tmux
 
 sudo apt install -y neofetch
-sudo echo "echo" >> /etc/update-motd.d/10-uname
-sudo echo "neofetch" >> /etc/update-motd.d/10-uname
+sudo rm -f /etc/update-motd.d/10-uname
+sudo touch /etc/update-motd.d/10-uname
+
+sudo cat << EOF > /etc/update-motd.d/10-uname
+#!/bin/sh
+echo
+$(neofetch)
+EOF
 
 sudo apt install -y mc
 
