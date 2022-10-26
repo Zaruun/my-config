d5e1c0f274e66f7e614498d8115ef0a03167d1af
Matt <mucha.mateusz@hotmail.com>
2022-10-26 15:31:40 +0200

    Update node-js-tree-sitter-cli-install.sh

--- scripts/nvim/node-js-tree-sitter-cli-install.sh
+++ scripts/nvim/node-js-tree-sitter-cli-install.sh
@@ -3,7 +3,7 @@
 
 wget https://nodejs.org/dist/v18.12.0/node-v18.12.0-linux-x64.tar.xz
 
-tar -xf node-v18.12.0-linux-x64
+tar -xf node-v18.12.0-linux-x64.tar.xz
 cd node-v18.12.0-linux-x64
 
 sudo cp -r ./{lib,share,include,bin} /usr
