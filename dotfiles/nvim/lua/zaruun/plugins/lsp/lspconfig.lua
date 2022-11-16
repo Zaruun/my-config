0699cc49f170676a6e32e054cd42d55ff53b6fe0
Matt <mucha.mateusz@hotmail.com>
2022-11-16 10:59:16 +0100

    jadi-language-server + black

--- dotfiles/nvim/lua/zaruun/plugins/lsp/lspconfig.lua
+++ dotfiles/nvim/lua/zaruun/plugins/lsp/lspconfig.lua
@@ -82,6 +82,12 @@ lspconfig["tailwindcss"].setup({
 	on_attach = on_attach,
 })
 
+-- configure jedi server
+lspconfig["jedi_language_server"].setup({
+	capabilities = capabilities,
+	on_attach = on_attach,
+})
+
 -- configure lua server (with special settings)
 lspconfig["sumneko_lua"].setup({
 	capabilities = capabilities,
