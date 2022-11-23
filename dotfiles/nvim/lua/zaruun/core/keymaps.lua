abe28b2ec8e83419585b2db6e1359c95a6a3be89
Matt <mucha.mateusz@hotmail.com>
2022-11-23 09:33:47 +0100

    Update keymaps.lua

--- dotfiles/nvim/lua/zaruun/core/keymaps.lua
+++ dotfiles/nvim/lua/zaruun/core/keymaps.lua
@@ -16,6 +16,9 @@ keymap.set("n", "<leader>nh", ":nohl<CR>")
 -- delete single character without copying into register
 keymap.set("n", "x", '"_x')
 
+-- delete selection without copying text
+keymap.set("v", "<leader>d", '"_d')
+
 -- increment/decrement numbers
 keymap.set("n", "<leader>+", "<C-a>") -- increment
 keymap.set("n", "<leader>-", "<C-x>") -- decrement
