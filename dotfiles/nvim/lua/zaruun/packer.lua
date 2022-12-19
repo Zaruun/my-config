9ddd248d1d22624b3ed1d28d6f302fb6b46e948d
Matt <mucha.mateusz@hotmail.com>
2022-12-19 22:26:52 +0100

    null-ls

--- dotfiles/nvim/lua/zaruun/packer.lua
+++ dotfiles/nvim/lua/zaruun/packer.lua
@@ -96,6 +96,10 @@ return packer.startup(function(use)
     }
   }
 
+  -- formatting & linting
+	use("jose-elias-alvarez/null-ls.nvim") -- configure formatters & linters
+	use("jayp0521/mason-null-ls.nvim") -- bridges gap b/w mason & null-ls
+
   -- auto closing
 	use("windwp/nvim-autopairs") -- autoclose parens, brackets, quotes, etc...
 	use({ "windwp/nvim-ts-autotag", after = "nvim-treesitter" }) -- autoclose tags
--- dotfiles/nvim/lua/zaruun/remap.lua
+++ dotfiles/nvim/lua/zaruun/remap.lua
@@ -1,5 +1,5 @@
 vim.g.mapleader = " "
-vim.keymap.set("n","<leader>pv", vim.cmd.Ex)
+vim.keymap.set("n", "<leader>pv", vim.cmd.Ex)
 
 -- move highlighted text
 vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
@@ -10,7 +10,7 @@ vim.keymap.set("n", "n", "nzzzv")
 vim.keymap.set("n", "N", "Nzzzv")
 
 -- paste over text w/o copying replacing text
-vim.keymap.set("x", "<leader>p", "\"_dP")
+vim.keymap.set("x", "<leader>p", '"_dP')
 
 -- replace current word in file
 vim.keymap.set("n", "<leader>s", [[:%s/\<<C-r><C-w>\>/<C-r><C-w>/gI<Left><Left><Left>]])
@@ -43,3 +43,8 @@ vim.keymap.set("n", "<leader>sv", "<C-w>v") -- split window vertically
 vim.keymap.set("n", "<leader>sh", "<C-w>s") -- split window horizontally
 vim.keymap.set("n", "<leader>se", "<C-w>=") -- make split windows equal width & height
 vim.keymap.set("n", "<leader>sx", ":close<CR>") -- close current split window
+
+-- formatting
+vim.keymap.set("n", "<leader>f", vim.lsp.buf.format)
+
+vim.keymap.set("n", "<leader>f", vim.lsp.buf.format)
