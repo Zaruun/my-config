992e35654ad10b06db2c201eaaefd67289f1a70b
Mateusz <mucha.mateusz@hotmail.com>
2024-01-02 21:07:56 +0100

    ansible / core tools / zsh

---
+++ Linux/updater.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+# Check if Ansible is installed 
+if [ $(dpkg-query -W -f='${Status}' ansible 2>/dev/null | grep -c "ok installed") -eq 0 ];
+then
+    echo "Ansible is not installed. Installing..."
+    
+    # Install Ansible
+    sudo apt update
+    sudo apt install -y ansible
+    
+    echo "Ansible installed successfully."
+else
+    echo "Ansible is already installed."
+fi
+
+# Run Ansible playbook
+ansible-playbook -i localhost _ansible/devenv.yaml --ask-become-pass
