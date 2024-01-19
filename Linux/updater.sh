6c686657da82dbaecc6b62a166e81e8fcae8a9a0
Mateusz <mucha.mateusz@hotmail.com>
2024-01-19 14:21:00 +0100

    zshrc updater as func

--- Linux/updater.sh
+++ Linux/updater.sh
@@ -38,4 +38,4 @@ echo ""
 ansible-playbook -i localhost _ansible/devenv.yaml --ask-become-pass
 
 # Update .zshrc updater.sh path to alias
-sed -i "s|#script_path_to_replace|sh $(pwd)/updater.sh|" ~/.zshrc
+sed -i "s|#script_path_to_replace|cd $(pwd) \&\& sh $(pwd)/updater.sh|" ~/.zshrc
