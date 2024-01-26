cd5d289fecf0379de1d0f75788393d41599e3440
Mateusz <mucha.mateusz@hotmail.com>
2024-01-26 14:59:16 +0100

    feat: nvim settings

--- Linux/dotfiles/nvim/lua/vim-options.lua
+++ Linux/dotfiles/nvim/lua/vim-options.lua
@@ -14,3 +14,11 @@ vim.keymap.set('n', '<leader>h', ':nohlsearch<CR>')
 vim.wo.number = true
 
 vim.opt.clipboard = "unnamed"
+
+vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
+vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv")
+
+vim.keymap.set("n", "<C-d>", "<C-d>zz")
+vim.keymap.set("n", "<C-u>", "<C-u>zz")
+vim.keymap.set("n", "n", "nzzzv")
+vim.keymap.set("n", "N", "Nzzzv")
