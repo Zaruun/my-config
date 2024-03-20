766038b96c8a5d3fcdf39f8e1d8731a5acd8c00f
Matt <mucha.mateusz@hotmail.com>
2024-03-20 09:11:51 +0100

    Update keymaps.lua

--- Linux/dotfiles/nvim/lua/config/keymaps.lua
+++ Linux/dotfiles/nvim/lua/config/keymaps.lua
@@ -1,8 +1,8 @@
-vim.keymap.set('n', '<c-j>', ':wincmd j<CR>')
-vim.keymap.set('n', '<c-h>', ':wincmd h<CR>')
-vim.keymap.set('n', '<c-l>', ':wincmd l<CR>')
+vim.keymap.set("n", "<c-j>", ":wincmd j<CR>")
+vim.keymap.set("n", "<c-h>", ":wincmd h<CR>")
+vim.keymap.set("n", "<c-l>", ":wincmd l<CR>")
 
-vim.keymap.set('n', '<leader>nh', ':nohlsearch<CR>')
+vim.keymap.set("n", "<leader>nh", ":nohlsearch<CR>")
 
 vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
 vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv")
@@ -16,7 +16,8 @@ vim.keymap.set("n", "<leader>s", [[:%s/\<<C-r><C-w>\>/<C-r><C-w>/gI<Left><Left><
 
 -- greatest remap ever
 vim.keymap.set("x", "<leader>p", [["_dP]])
-vim.keymap.set({"n", "v"}, "<leader>d", [["_d]])
+vim.keymap.set({ "n", "v" }, "<leader>d", [["_d]])
+vim.keymap.set("n", "<Leader>dw", [[:normal! viw"_d<CR>]])
 
 -- better indenting
 vim.keymap.set("v", "<", "<gv")
